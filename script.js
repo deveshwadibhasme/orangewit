ae67b54e977f6236987cd4bf269622dc72c18391
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-08-17 19:09:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const menuIcon = document.querySelector('.menu-icon')
 const dropMenu = document.querySelector('.drop-menu')
+const closeMenu = document.querySelector('.close-btn')
 
 menuIcon.addEventListener('click', () =>{
-    dropMenu.classList.toggle('open')
+    dropMenu.classList.add('open')
+})
+closeMenu.addEventListener('click', () =>{
+    dropMenu.classList.remove('open')
 })
 menuIcon.addEventListener('touchup', () =>{
     dropMenu.classList.toggle('open')
+})
+closeMenu.addEventListener('touchup', () =>{
+    dropMenu.classList.remove('open')
 })
